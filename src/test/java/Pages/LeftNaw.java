d0788f3ef1c120e0ea152d1ecd234cbb6825a814
muazatmaca <muaz5447@gmail.com>
2023-04-07 22:22:48 +0300

    leftnaw oluşturuldu

--- src/test/java/Pages/LeftNaw.java
+++ src/test/java/Pages/LeftNaw.java
@@ -1,8 +1,18 @@
 package Pages;
 
-public class LeftNaw {
+import Utility.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class LeftNaw extends Parent{
 
+    public LeftNaw() {
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//span[text()='Setup'])[1]")
+    public WebElement setupButton;
 
 
 
